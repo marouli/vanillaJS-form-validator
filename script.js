d011ba3c76cb04e550952e929bb40aea44c31c5f
Maria Sidiropoulou <maroulisid@gmail.com>
2020-03-06 15:13:34 +0100

    refactor check require

--- script.js
+++ script.js
@@ -21,33 +21,21 @@ function isValidEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
+function checkRequired(inputArray) {
+  inputArray.forEach((input) => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    }
+  });
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  if (username.value === '') {
-    showError(username, 'Username is required');
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === '') {
-    showError(email, 'Email is required');
-  } else if(!isValidEmail(email.value)) {
-    showError(email, 'Email is not valid')
-  } else {
-    showSuccess(email);
-  }
-
-  if (password.value === '') {
-    showError(password, 'Password is required');
-  } else {
-    showSuccess(password);
-  }
-
-  if (password2.value === '') {
-    showError(password2, 'Password confirmation is required');
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
 
 })
